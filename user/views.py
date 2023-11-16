bc9f6f4ca57c6e630838f0b47fcab34925a11a7a
Daniel ferreira nunes <segundacontafd@gmail.com>
2023-11-16 09:19:20 -0300

    update model and test the view

--- user/views.py
+++ user/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.views import View
 from user.models import User
 from user.serializer import UserSerializer
+import django.core.exceptions as exceptions 
 import hashlib
 from rest_framework.response import Response
 from rest_framework import viewsets
 # Create your views here.
 class UserView(viewsets.ViewSet):
     def create(self, request):
-        password = hashlib.sha256(request.data.get('password',None).enconde('utf-8'))
+        password = hashlib.sha256(request.data.get('password',None).enconde(encoding='UTF-8'))
         user = UserSerializer(data={
             'email': request.data.get('email',None),
             'password': str(password.hexdigest())
