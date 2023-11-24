4d225f02deced139d8194027dd88db82c740726e
Daniel ferreira nunes <segundacontafd@gmail.com>
2023-11-24 10:35:23 -0300

    create telalogin logic create view for telalogin

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
+from django.urls import path
 from user.views import UserView
+from .views import LoginView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
- 
+
 urlpatterns = [
     path('create/', UserView.as_view(), name='user_create'),
     path('createtoken/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("refreshtoken/", TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,7 +8,7 @@ from user.serializer import UserSerializer  # replace 'user' with the name of yo
 from user.models import User  # replace 'user' with the name of your app
 
 class UserView(APIView):
-    def post(self, request):
+    def CreateUser(self, request):
         user_serializer = UserSerializer(data=request.data)
         if user_serializer.is_valid():
             user = user_serializer.save()
@@ -21,5 +22,15 @@ class UserView(APIView):
         else:
             return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def telalogin(self,request):
-        
+    def telalogin(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+            'refresh': str(refresh),
+            'acess': str(refresh.acess_token),
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({'erro': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
